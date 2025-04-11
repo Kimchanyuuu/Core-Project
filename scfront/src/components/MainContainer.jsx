0447d92530e33d1c557265106f40c7603e94e952
kcy <ka5954@naver.com>
2025-04-11 11:36:06 +0900

    프런트첫번째

--- scfront/src/components/MainContainer.jsx
+++ scfront/src/components/MainContainer.jsx
@@ -1,49 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../css/maincontainer.css";
-import Menu from '../img/menu.png'
-import Close from '../img/close.png'
+import Menu from "../img/menu.png";
+import Close from "../img/close.png";
+import Login from "./SidePage";
+import mainImage from "../img/gpt.png"
 
 const MainContain = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [visible, setVisible] = useState(false);
   const nav = useNavigate();
 
-  return(
-  <main className="subMain">
-    <header id="subHeader" className="mainHeader">
+  const toggleMenu = () => {
+    setVisible(true);
+    setTimeout(() => setMenuOpen(true), 10);
+  };
+
+  const closeMenuAndGoHome = () => {
+    console.log("X 버튼이 클릭되었습니다!");
+    setMenuOpen(false);
+    setTimeout(() => setVisible(false), 300);
+  };
+
+  return (
+    <main className="subMain">
+      <header className="mainHeader">
         <div className="subHeaderWrapper">
-            
-            <button className="headerMenu">
-                <img src={Menu} alt="menu" width="36px" />
+          <button onClick={toggleMenu} className="headerMenu">
+            <img src={Menu} alt="menu" width="36px" />
+          </button>
+        </div>
+      </header>
+
+      {visible && (
+        <div className={`sideMenu ${menuOpen ? "open" : ""}`}>
+          <div className="closeButton">
+            <button onClick={closeMenuAndGoHome}>
+              <img src={Close} alt="close" width="36px" />
             </button>
-         </div>
-        {/* <div className="gnbBackground">
-          <div className="gnbslider">
-            <header className="gnbHeader">
-              <button className="gnbClose">
-                <img src={Close} alt="menu" width="32px" />
-              </button>
-            </header>
-
-            <nav className="gnbNav">
-              로그인
-            </nav>
           </div>
-        </div>  */}
-    </header>
-   {/* <div id="MainPage">
-    <section className="mainVisual">
-      <div className="inner">
-          <h1>시세조회 할 <br/>
-            휴대폰정보를 선택해주세요.</h1>
-            <hr />
-      </div>
-    </section>
-          
-    </div>        */}
-  </main>
+          <Login />
+        </div>
+      )}
 
- 
-  )
+      <div id="MainPage">
+        <section className="mainVisual">
+          <div className="inner">
+            <h1 style={{ textAlign: "center" }}>
+              <br />
+              <b>중고폰, 지금 얼마일까?</b>
+              <br />
+              궁금한 중고폰 시세,
+              <br />
+              브랜드부터 모델, 용량까지
+              <br />
+              클릭 몇 번이면 확인 끝!
+              <br />
+            </h1>
+            <p></p>
+            <img src={mainImage} className="mainImage"></img>
+          </div>
+        </section>
+      </div>
+      <section className="mainSection1">
+        <div className="innerSection">
+          <a className="bm1">
+            <span>시세조회</span>
+          </a>
+          <a className="bm1">
+            <span>ATM</span>
+          </a>
+          <a className="bm1">
+            <span>이용방법</span>
+          </a>
+        </div>
+      </section>
+      <section>
+        <div className="inner">
+          <div>
+            <img src={mainImage} className="mainImage"></img>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default MainContain;
